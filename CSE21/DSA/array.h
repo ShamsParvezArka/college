8d51ede26f0737db0b25805736ce7492a9fccb22
ShamsParvezArka <parvez6826@gmail.com>
2024-02-05 19:41:53 +0600

    [impl] push_back, push_front, pop_back, pop_front

--- CSE21/DSA/array.h
+++ CSE21/DSA/array.h
@@ -4,6 +4,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MOD_ARRAY_AT(a, i) (a)->items[(i)]
+#define ARRAY_AT(a, i) (a).items[(i)]
+
 typedef struct array {
 	int *items;
 	size_t count;
@@ -11,8 +14,11 @@ typedef struct array {
 } array;
 
 array array_init(size_t size);
-void array_push(array *arr, int item);
+void array_push_back(array *arr, int item);
+void array_push_front(array *arr, int item);
 void array_insert(array *arr, size_t index, int item);
+void array_pop_front(array *arr);
+void array_pop_back(array *arr);
 void array_fill(array *arr, size_t begin, size_t end, int c);
 void array_erase(array *arr, size_t index);
 size_t array_search(array haystack, int needle);
@@ -28,23 +34,59 @@ array array_init(size_t size)
 		.count = 0,
 		.size = size
 	};
-	memset(arr.items, 0, sizeof(*arr.items)*arr.size);
+	memset(arr.items, '\0', sizeof(*arr.items)*arr.size);
 	return arr;
 }
 
-void array_push(array *arr, int item)
+void array_push_front(array *arr, int item)
 {
-	if (arr->count >= arr->size - 1) {
+	if (arr->count >= arr->size) {
 		printf("%s: error: [overflow] heap memory limit reached\n", __FILE__);
 		exit(1);
 	}
-	arr->items[arr->count] = item;
+	for (size_t i = arr->count; i > 0; i--) {
+		MOD_ARRAY_AT(arr, i) = MOD_ARRAY_AT(arr, i - 1);
+	}
+	MOD_ARRAY_AT(arr, 0) = item;
 	arr->count++;
+}
+
+void array_push_back(array *arr, int item)
+{
+	if (arr->count >= arr->size) {
+		printf("%s: error: [overflow] heap memory limit reached\n", __FILE__);
+		exit(1);
+	}
+	MOD_ARRAY_AT(arr, arr->count) = item;
+	arr->count++;
+}
+
+void array_pop_front(array *arr)
+{
+	if (arr->count == 0) {
+		printf("%s: error: [underflow] heap memory is empty\n", __FILE__);
+		exit(1);
+	}
+	for (size_t i = 0; i < arr->size; i++) {
+		MOD_ARRAY_AT(arr, i) = MOD_ARRAY_AT(arr, i + 1);
+	}
+	MOD_ARRAY_AT(arr, arr->count) = '\0';
+	arr->count--;
+}
+
+void array_pop_back(array *arr)
+{
+	if (arr->count == 0) {
+		printf("%s: error: [underflow] heap memory is empty\n", __FILE__);
+		exit(1);
+	}
+	MOD_ARRAY_AT(arr, arr->count) = '\0';
+	arr->count--;
 }
 
 void array_insert(array *arr, size_t index, int item)
 {
-	if (arr->count >= arr->size - 1) {
+	if (arr->count >= arr->size) {
 		printf("%s: error: [overflow] heap memory limit reached\n", __FILE__);
 		exit(1);
 	}
@@ -54,19 +96,13 @@ void array_insert(array *arr, size_t index, int item)
 	}
 	if (arr->count > index) {
 		for (size_t i = arr->count; i > index; i--) {
-			arr->items[i] = arr->items[i - 1];
+			MOD_ARRAY_AT(arr, i) = MOD_ARRAY_AT(arr, i - 1);
 		}
-		arr->items[index] = item;
+		MOD_ARRAY_AT(arr, index) = item;
 	}
 	if (arr->count < index && arr->size > index)
-		arr->items[index] = item;
+		MOD_ARRAY_AT(arr, index) = item;
 	arr->count++;
-}
-
-void array_fill(array *arr, size_t begin, size_t end, int c)
-{
-	for (size_t i = begin; i <= end; i++)
-		arr->items[i] = c;
 }
 
 void array_erase(array *arr, size_t index)
@@ -76,15 +112,22 @@ void array_erase(array *arr, size_t index)
 		exit(1);
 	}
 	for (size_t i = index; i < arr->size; i++) {
-		arr->items[i] = arr->items[i + 1];
+		MOD_ARRAY_AT(arr, i) = MOD_ARRAY_AT(arr, i + 1);
 	}
+	MOD_ARRAY_AT(arr, arr->count);
 	arr->count--;
+}
+
+void array_fill(array *arr, size_t begin, size_t end, int c)
+{
+	for (size_t i = begin; i <= end; i++)
+		MOD_ARRAY_AT(arr, i) = c;
 }
 
 size_t array_search(array haystack, int needle)
 {
 	for (size_t i = 0; i < haystack.count; i++) {
-		if (haystack.items[i] == needle)
+		if (ARRAY_AT(haystack, i) == needle)
 			return i;
 	}
 	return haystack.size;
